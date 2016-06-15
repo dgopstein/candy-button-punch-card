f9c540305af375b8d65652d24c0025cd71262ce8
Dan Gopstein <dan@gopstein.com>
2016-06-15 02:57:30 -0400

    Compiles and runs and does nothing

--- style_identity_matrix.f
+++ style_identity_matrix.f
@@ -1,5 +1,9 @@
 C  MAKE V AN IDENTITY MATRIX
+      PROGRAM MAIN
+      DIMENSION V(10,10)
+      N =9 
       DO 14 I = 1,N
          DO 12 J = 1,N
-   12       V(I,J) = 0.0
-   14     V(I,I) = 1.0
+   12      V(I,J) = 0.0
+   14    V(I,I) = 1.0
+      END
